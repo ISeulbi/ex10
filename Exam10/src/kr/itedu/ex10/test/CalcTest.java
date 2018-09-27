249d80fadbc84c356fd14fda100484838a228f8c
USER <USER@USER-PC>
2018-09-27 14:18:31 +0900

    3. Calc, CalcTest push

---
+++ Exam10/src/kr/itedu/ex10/test/CalcTest.java
@@ -0,0 +1,18 @@
+package kr.itedu.ex10.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import kr.itedu.ex10.Calc;
+
+class CalcTest {
+
+	@Test
+	void test() {
+		assertEquals(3, Calc.sum(1, 2));
+		assertEquals(7, Calc.sum(0, 7));
+		assertEquals(10, Calc.sum(11, -1));
+	}
+
+}
